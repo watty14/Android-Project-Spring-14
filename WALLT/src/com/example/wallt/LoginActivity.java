f8131e2d57851bfab9f7899c0a6ca416d323d2e6
Thomas Harris <teharris07@gmail.com>
2014-02-12 11:37:36 -0500

    included a crude version of login ability

--- WALLT/src/com/example/wallt/LoginActivity.java
+++ WALLT/src/com/example/wallt/LoginActivity.java
@@ -4,6 +4,7 @@
 package com.example.wallt;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -14,6 +15,26 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         // setting default screen to login.xml
         setContentView(R.layout.login_activity);
+        
+        TextView login = (TextView) findViewById(R.id.btnLogin);
+        
+        final TextView userName = (TextView) findViewById(R.id.usernameField);
+        final TextView passWord = (TextView) findViewById(R.id.passwordField);
+        
+        login.setOnClickListener(new View.OnClickListener() {
+ 
+            public void onClick(View v) {
+            	if (userName.getText().toString().equals("user") 
+            			&& passWord.getText().toString().equals("password")) {
+	            	Intent i = new Intent(getApplicationContext(), MainHub.class);
+	          	  	startActivity(i);
+            	} else {
+            		passWord.setText("");
+            		Intent i = new Intent(getApplicationContext(), LoginFail.class);
+	          	  	startActivity(i);
+            	}
+            }
+        });
  
         TextView back = (TextView) findViewById(R.id.backToMain);
  
---
+++ WALLT/src/com/example/wallt/LoginFail.java
@@ -0,0 +1,15 @@
+package com.example.wallt;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class LoginFail extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.loginfail_activity);
+	}
+
+}
