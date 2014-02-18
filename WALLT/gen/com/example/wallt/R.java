72bbd105c6623f183d4d1d0c3628f71c04efa161
ycho46 <ycho46@gmail.com>
2014-02-18 11:18:45 -0500

    commit

--- WALLT/gen/com/example/wallt/R.java
+++ WALLT/gen/com/example/wallt/R.java
@@ -29,32 +29,65 @@ public final class R {
         public static final int wallt=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int backToMain=0x7f080002;
-        public static final int btnLogin=0x7f080001;
-        public static final int imageView1=0x7f080004;
-        public static final int link_Register=0x7f080005;
-        public static final int link_login=0x7f080003;
-        public static final int password=0x7f080000;
+        public static final int action_settings=0x7f080010;
+        public static final int backToMain=0x7f080005;
+        public static final int btnLogin=0x7f080004;
+        public static final int create=0x7f08000b;
+        public static final int databaseFeed=0x7f08000c;
+        public static final int emailAddress=0x7f080009;
+        public static final int emailField=0x7f08000a;
+        public static final int imageView1=0x7f08000e;
+        public static final int linearLayout1=0x7f080006;
+        public static final int link_Register=0x7f08000f;
+        public static final int link_login=0x7f08000d;
+        public static final int password=0x7f080002;
+        public static final int passwordField=0x7f080003;
+        public static final int userName=0x7f080007;
+        public static final int userNameField=0x7f080008;
+        public static final int username=0x7f080000;
+        public static final int usernameField=0x7f080001;
     }
     public static final class layout {
         public static final int login_activity=0x7f030000;
-        public static final int splash_activity=0x7f030001;
-        public static final int welcome_activity=0x7f030002;
+        public static final int loginfail_activity=0x7f030001;
+        public static final int mainhub_activity=0x7f030002;
+        public static final int register_activity=0x7f030003;
+        public static final int registrationfail_activity=0x7f030004;
+        public static final int registrationfeed_activity=0x7f030005;
+        public static final int splash_activity=0x7f030006;
+        public static final int welcome_activity=0x7f030007;
     }
     public static final class menu {
-        public static final int splash_activiy=0x7f070000;
+        public static final int login_fail=0x7f070000;
+        public static final int main_hub=0x7f070001;
+        public static final int register=0x7f070002;
+        public static final int registration_fail=0x7f070003;
+        public static final int registration_feed=0x7f070004;
+        public static final int splash_activiy=0x7f070005;
     }
     public static final class string {
         public static final int Login=0x7f050003;
         public static final int Register=0x7f050004;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050001;
-        public static final int backToMain=0x7f050009;
-        public static final int email=0x7f050007;
+        public static final int backToMain=0x7f05000c;
+        public static final int createAccount=0x7f050009;
+        public static final int email=0x7f050008;
+        public static final int fullName=0x7f05000a;
+        public static final int hello_world=0x7f05000f;
         public static final int id=0x7f050006;
+        public static final int login_fail=0x7f050011;
         public static final int logo=0x7f050005;
-        public static final int pwd=0x7f050008;
+        public static final int pwd=0x7f05000b;
+        public static final int registration_failed=0x7f050012;
+        public static final int successful_login=0x7f050010;
+        public static final int title_activity_login=0x7f05000e;
+        public static final int title_activity_login_fail=0x7f050014;
+        public static final int title_activity_main_hub=0x7f050013;
+        public static final int title_activity_register=0x7f05000d;
+        public static final int title_activity_registration_fail=0x7f050016;
+        public static final int title_activity_registration_feed=0x7f050015;
+        public static final int userName=0x7f050007;
         public static final int welcome=0x7f050000;
     }
     public static final class style {
